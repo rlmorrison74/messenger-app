8580db3a4118cc5a75a92f9daff6cc829dfbdf79
Ray Morrison <rlmorrison.74@gmail.com>
2021-11-19 12:16:05 -0500

    dependencies added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,9 @@
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY
+
 export default function App() {
     return (
         <div>
